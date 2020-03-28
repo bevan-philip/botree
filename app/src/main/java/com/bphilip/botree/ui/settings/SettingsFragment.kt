5ad799863a02f38ba92efbef904c4301031e3371
Bevan Philip <git@bphilip.uk>
2020-03-28 19:24:32 +0000

    Fix some utterly borked file name issues.

--- app/src/main/java/com/bphilip/botree/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/bphilip/botree/ui/settings/SettingsFragment.kt
@@ -21,8 +21,8 @@ class SettingsFragment : Fragment() {
     ): View? {
         settingsViewModel =
             ViewModelProviders.of(this).get(SettingsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_reflections, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        val root = inflater.inflate(R.layout.fragment_settings, container, false)
+        val textView: TextView = root.findViewById(R.id.text_notifications)
         settingsViewModel.text.observe(this, Observer {
             textView.text = it
         })
--- app/src/main/java/com/bphilip/botree/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/bphilip/botree/ui/settings/SettingsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class SettingsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
 }
