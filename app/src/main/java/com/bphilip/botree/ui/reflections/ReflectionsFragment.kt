90e0f3e21873e1171fc86a979c6a1569b7198b84
Bevan Philip <arrivance@gmail.com>
2020-03-30 20:32:01 +0100

    Initial working version of Kotlin Room demo.

--- app/src/main/java/com/bphilip/botree/ui/reflections/ReflectionsFragment.kt
+++ app/src/main/java/com/bphilip/botree/ui/reflections/ReflectionsFragment.kt
@@ -1,18 +1,29 @@
 package com.bphilip.botree.ui.reflections
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
-import com.bphilip.botree.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bphilip.botree.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ReflectionsFragment : Fragment() {
 
     private lateinit var reflectionsViewModel: ReflectionsViewModel
+    private val newWordActivityRequestCode = 1
+    private lateinit var wordViewModel: WordViewModel
+    private var mContext: Context? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +33,53 @@ class ReflectionsFragment : Fragment() {
         reflectionsViewModel =
             ViewModelProviders.of(this).get(ReflectionsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_reflections, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        reflectionsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter(activity as Context)
+        adapter.notifyDataSetChanged()
+        recyclerView?.adapter = adapter
+        recyclerView?.layoutManager = LinearLayoutManager(mContext as Context)
+
+        wordViewModel = ViewModelProviders.of(this).get(WordViewModel::class.java)
+        wordViewModel.allWords.observe(this, Observer { words ->
+            // Update the cached copy of the words in the adapter.
+            words?.let { adapter.setWords(it) }
+        })
+
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
+        fab?.setOnClickListener {
+            val intent = Intent(mContext, NewWordActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.getStringExtra(NewWordActivity.EXTRA_REPLY)?.let {
+                val word = Word(it)
+                wordViewModel.insert(word)
+            }
+        } else {
+            Toast.makeText(
+                mContext,
+                R.string.empty_not_saved,
+                Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        mContext = null
     }
 }
