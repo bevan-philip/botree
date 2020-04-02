12d58ae3ae2296f8e11ef4e9c80f77b0aaf3a1e8
Bevan Philip <git@bphilip.uk>
2020-04-02 20:27:38 +0100

    Add internal support for changing dates.

--- app/src/main/java/com/bphilip/botree/WordDao.kt
+++ app/src/main/java/com/bphilip/botree/WordDao.kt
@@ -5,13 +5,14 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import java.util.*
 
 
 @Dao
 interface WordDao {
 
-    @Query("SELECT * from word_table ORDER BY date DESC")
-    fun getAlphabetizedWords(): LiveData<List<Word>>
+    @Query("SELECT * from word_table WHERE date BETWEEN :start AND :end ORDER BY date DESC")
+    fun getAlphabetizedWords(start : Long, end : Long): LiveData<List<Word>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(word: Word)
--- app/src/main/java/com/bphilip/botree/WordRepository.kt
+++ app/src/main/java/com/bphilip/botree/WordRepository.kt
@@ -1,16 +1,37 @@
 package com.bphilip.botree
 
+import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalDateTime
+import org.threeten.bp.ZoneId
 
 // Declares the DAO as a private property in the constructor. Pass in the DAO
 // instead of the whole database, because you only need access to the DAO
 class WordRepository(private val wordDao: WordDao) {
 
-    // Room executes all queries on a separate thread.
-    // Observed LiveData will notify the observer when the data has changed.
-    val allWords: LiveData<List<Word>> = wordDao.getAlphabetizedWords()
+    private val startTime : MutableLiveData<LocalDateTime> = MutableLiveData()
+    private val endTime : MutableLiveData<LocalDateTime> = MutableLiveData()
+
+    var allWords: LiveData<List<Word>> = Transformations.switchMap(endTime) {
+        wordDao.getAlphabetizedWords(
+            conv(startTime.value) as Long,
+            conv(endTime.value) as Long
+        )
+    }
 
     suspend fun insert(word: Word) {
         wordDao.insert(word)
     }
+
+    fun changeTime(stime : LocalDateTime, etime: LocalDateTime) {
+        startTime.value = stime
+        endTime.value = etime
+    }
+}
+
+fun conv(date: LocalDateTime?): Long? {
+    return date?.atZone(ZoneId.systemDefault())?.toInstant()?.toEpochMilli()
 }
