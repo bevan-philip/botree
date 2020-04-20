be0466d2d76cd42fcfedb829f17f479cf8ea5fee
Bevan Philip <arrivance@gmail.com>
2020-04-20 19:23:55 +0100

    Improve exporting functionality using Kotlin's sealed classes.

--- app/src/main/java/com/bphilip/botree/Reflection.kt
+++ app/src/main/java/com/bphilip/botree/Reflection.kt
@@ -3,20 +3,45 @@ package com.bphilip.botree
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import androidx.room.TypeConverter
 import org.threeten.bp.*
-import java.util.*
 
+/**
+ * Uses Kotlin's sealed classes to allow the data classes to have common properties.
+ * We use this to have a common export class for the data classes.
+ * See: https://kotlinlang.org/docs/reference/whatsnew11.html#sealed-and-data-classes
+ */
+sealed class Exportable {
+    // Exports a record as a CSV.
+    fun asCSV(exportable: Exportable) : String {
+        return when (exportable) {
+            is Reflection -> String.format("%s, %s, %s\n",
+                exportable.id.toString(),
+                exportable.reflection,
+                exportable.date.toString()
+            )
+            is Meditation -> String.format("%s, %s, %s\n",
+                exportable.id.toString(),
+                exportable.duration.toString(),
+                exportable.date.toString()
+            )
+        }
+    }
+
+    // Exports the header for a record type.
+    fun headerExport(exportable: Exportable): String {
+        return when (exportable) {
+            is Reflection -> "id, reflection, date\n"
+            is Meditation -> "id, duration, date\n"
+        }
+    }
+}
 // The fields for reflections.
 @Entity(tableName = "reflections_table")
 data class Reflection(
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id")  val id : Int = 0,
     @ColumnInfo(name = "reflection") val reflection: String,
     @ColumnInfo(name = "date") val date : LocalDateTime?
-)
-{
-    fun asCSV() : String { return String.format("%s, %s, %s\n", id.toString(), reflection, date.toString())}
-}
+) : Exportable()
 
 // The fields of meditations.
 @Entity(tableName = "meditations_table")
@@ -26,7 +51,4 @@ data class Meditation(
     // We don't store the time for meditations so it is easier to determine whether the user
     // meditated on a certain day (as otherwise, we'd have to search from the start to the end).
     @ColumnInfo(name = "date") val date : LocalDate?
-)
-{
-    fun asCSV() : String { return String.format("%s, %s, %s\n", id.toString(), duration.toString(), date.toString())}
-}
+) : Exportable()
