618f5e578af4e5085adc4f8d0da1ebcc562de06d
Bevan Philip <arrivance@gmail.com>
2020-05-10 17:46:35 +0100

    add reboot support to reboot alarm persistence.

--- app/src/main/java/com/bphilip/botree/BootBroadcastReciever.kt
+++ app/src/main/java/com/bphilip/botree/BootBroadcastReciever.kt
@@ -3,13 +3,15 @@ package com.bphilip.botree
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 
 class BootBroadcastReciever : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == "android.intent.action.BOOT_COMPLETED") {
+        if (intent.action == "android.intent.action.BOOT_COMPLETED" || intent.action == "android.intent.action.QUICKBOOT_POWERON") {
             val meditationAlarm = MeditationAlarm()
             meditationAlarm.startAlarmBroadcastReceiver(context)
+            Log.i("BootBroadcastReceiver", "Restarting alarm")
         }
     }
 }
